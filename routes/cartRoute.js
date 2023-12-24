dc44e8b3825707b9d0b9745bc8f344037f80c109
lokmanzeddoun <l.zeddoun@esi-sba.dz>
2023-12-24 23:20:34 +0100

    Adding Coupon And Shopping Cart

---
+++ routes/cartRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const {
+	addProductToCart,
+	getLoggedUserCart,
+	removeProductFromCart,
+	clearCart,
+	updateQuantity,
+	applyCoupon
+} = require("../services/cartService");
+const authService = require("../services/authService");
+
+const router = express.Router();
+
+router.use(authService.protect, authService.allowedTo("user"));
+
+router
+	.route("/")
+	.post(addProductToCart)
+	.get(getLoggedUserCart)
+	.delete(clearCart);
+	router.put("/applyCoupon",applyCoupon)
+router.route("/:itemId").delete(removeProductFromCart).put(updateQuantity);
+
+module.exports = router;
